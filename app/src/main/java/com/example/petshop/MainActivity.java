52ad308f47b81b25488203aad7c866379bcc1a51
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2023-01-22 18:06:06 +0800

    detail design fix

--- app/src/main/java/com/example/petshop/MainActivity.java
+++ app/src/main/java/com/example/petshop/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnGetStart = findViewById(R.id.btnGetStart);
+        btnGetStart = findViewById(R.id.button2);
         btnGetStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
