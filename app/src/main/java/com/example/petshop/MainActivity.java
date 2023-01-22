44f676c0af3a58b7c45eec000d3b00cdba35aee0
Wahgun <wahyugunawaniputu@gmail.com>
2023-01-22 17:45:17 +0800

    test

--- app/src/main/java/com/example/petshop/MainActivity.java
+++ app/src/main/java/com/example/petshop/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.petshop;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+       }
+
